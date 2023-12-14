8ee0f0ddeac0d1a9c0c0992e351a4d4e91e54300
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-14 08:00:14 +0300

    update trigger reset logic

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -9,7 +9,7 @@ BEFORE UPDATE
 ON users FOR EACH ROW
 BEGIN
         IF NEW.email <> OLD.email THEN
-                SET NEW.valid_email = NOT NEW.valid_email;
+                SET NEW.valid_email = 0;
         END IF;
 END;
 $$
