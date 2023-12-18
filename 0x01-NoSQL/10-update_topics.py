3fb5c94cc250980e68ed7cf444e4264a06564a46
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-18 16:15:44 +0300

    add 10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module tests updating documents in mongodb
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    changes all topics of a school document based on the name
+    """
+    mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
+
+
+if __name__ == "__main__":
+    pass
