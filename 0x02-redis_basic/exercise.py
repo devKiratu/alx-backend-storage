19e65542ba07b8a0b4f8f308a878f0ba7fae98f3
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-20 12:32:42 +0300

    add solution to task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!usr/bin/env python3
+"""
+This module defines a cache layer built on top of Redis
+"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """
+    This is a cache implementation based on Redis
+    """
+    def __init__(self):
+        """Initializes a redis instance"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int, float, bytes]) -> str:
+        """
+        stores data in a Redis database using a random key and returns
+        the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+
+        return key
