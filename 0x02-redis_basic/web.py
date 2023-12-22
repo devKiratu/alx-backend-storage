b605afcb8228d6c401d9fd5e4d35d2f5d71573ac
Christopher Ndung'u <dev.kiratu@gmail.com>
2023-12-22 09:34:41 +0300

    remove decorator implementation

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,47 +6,27 @@ HTML content of a particular URL and returns it.
 """
 import requests
 import redis
-from functools import wraps
-from typing import Callable
 
 cache = redis.Redis()
 
 
-def access_count(method: Callable) -> Callable:
-    """
-    decorator for counting access frequency and caching it for 10s
-    """
-    @wraps(method)
-    def wrapper(url):
-        """decorator implementation"""
-        # check if there is cached content and return it for slow connection
-        content_key = "html:{}".format(url)
-        counter_key = "count:{}".format(url)
-        cache.incr(counter_key)
-
-        data = cache.get(content_key)
-        if data:
-            return data.decode("utf-8")
-
-        # cache is expired, fetch new content and cache it
-        fresh_content = method(url)
-        cache.set(content_key, fresh_content, ex=10)
-        return fresh_content
-
-    return wrapper
-
-
-@access_count
 def get_page(url: str) -> str:
     """
     tracks how many times a particular URL was accessed in the key
     "count:{url}" and cache the result with an expiration time of 10 seconds.
     """
+    content_key = "html:{}".format(url)
+    counter_key = "count:{}".format(url)
+    # increment url counter
+    cache.incr(counter_key)
+
+    # check if there is cached content and return it for slow connection
+    data = cache.get(content_key)
+    if data:
+        return data.decode("utf-8")
+
+    # cache is expired, fetch new content and cache it
     response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    url = 'http://slowwly.robertomurray.co.uk'
-    results = get_page(url)
-    print(results)
+    fresh_content = response.text
+    cache.set(content_key, fresh_content, ex=10)
+    return fresh_content
